5d041b772711ff51655c8afc18d56bb67b58b867
Paul Gallas <pjg12@students.uwf.edu>
2026-02-24 17:53:03 -0600

    OMG, I almost lost everything.

--- Eternity/src/main/java/eternity/FrameNewClass.java
+++ Eternity/src/main/java/eternity/FrameNewClass.java
@@ -1,9 +1,29 @@
 package eternity;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class FrameNewClass extends JFrame {
     private final DataQuery dataQuery;
